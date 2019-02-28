d261dc71727dad719f23a9d64e82de6b58b3dba7
Udippuy <carlo.comis@gmail.com>
2019-02-28 12:43:28 +0100

    added reverse driving using streams

--- vehicle-data-generator/index.js
+++ vehicle-data-generator/index.js
@@ -9,10 +9,14 @@ If you're up for it
 const csvParse      = require ( "csv-parse")
 const fs            = require ( "fs")
 const Writable      = require ("stream").Writable
+const Readable      = require ("stream").Readable
+
+const fsReverse		= require("fs-reverse");
+const CombinedStream = require('combined-stream');
 
 const slowConnection = {
-	minDelay: 100,
-	range: 500
+	minDelay: 0,
+	range: 0
 }
 
 // NATS Server is a simple, high performance open source messaging system
@@ -30,55 +34,42 @@ const nats = NATS.connect({
 	json: true
 });
 
+let i = 0
+
+const outStream = (vehicleName) => new Writable({
+	objectMode: true,
+	write(obj, enc, cb) {
+		// setTimeout in this case is there to emulate real life situation
+		// data that came out of the vehicle came in with irregular interval
+		// Hence the Math.random() on the second parameter
+		setTimeout(() => {
+
+			i++
+			if((i % 100) === 0) {
+				console.log(`vehicle ${vehicleName} sent have sent ${i} messages`)
+				console.log(obj);
+			}
+
+			// The first parameter on this function is topics in which data will be broadcasted
+			// it also includes the vehicle name to segregate data between different vehicle
+			nats.publish(`nats-dev.vehicle-data.${vehicleName}`, obj, () => {
+				// I've replaced the original callback with a lambda that fires the callback
+				// after a random delay- this should simulate what happens on a slow connection
+				setTimeout(cb, Math.ceil(Math.random() * slowConnection.range) + slowConnection.minDelay);
+			});
+
+		}, Math.ceil(Math.random() * 150))
+	}
+});
+
 // This function will start reading out csv data from file and publish it on nats
-const readOutLoud = (vehicleName) => {
+const readOutLoud = (vehicleName, fileStream) => {
 	// Read out meta/route.csv and turn it into readable stream
-	const fileStream = fs.createReadStream("./meta/route.csv")
-	// =========================
-	// Question Point 1:
-	// What's the difference betweeen fs.createReadStream, fs.readFileSync, and fs.readFileAsync?
-	// And when to use one or the others
-	// =========================
-
-	// Now comes the interesting part,
-	// Handling this filestream requires us to create pipeline that will transform the raw string
-	// to object and sent out to nats
-	// The pipeline should looks like this
-	//
-	//  File -> parse each line to object -> published to nats
-	//
-
-	let i = 0
-
 	return (fileStream
 		// Filestream piped to csvParse which accept nodejs readablestreams and parses each line to a JSON object
 		.pipe(csvParse({ delimiter: ",", columns: true, cast: true }))
 		// Then it is piped to a writable streams that will push it into nats
-		.pipe(new Writable({
-			objectMode: true,
-			write(obj, enc, cb) {
-				// setTimeout in this case is there to emulate real life situation
-				// data that came out of the vehicle came in with irregular interval
-				// Hence the Math.random() on the second parameter
-				setTimeout(() => {
-
-					i++
-					if((i % 100) === 0) {
-						console.log(`vehicle ${vehicleName} sent have sent ${i} messages`)
-						console.log(obj);
-					}
-
-					// The first parameter on this function is topics in which data will be broadcasted
-					// it also includes the vehicle name to segregate data between different vehicle
-					nats.publish(`nats-dev.vehicle-data.${vehicleName}`, obj, ()=>{
-						// I've replaced the original callback with a lambda that fires the callback
-						// after a random delay- this should simulate what happens on a slow connection
-						setTimeout(cb, Math.ceil(Math.random() * slowConnection.range) + slowConnection.minDelay);
-					});
-
-				}, Math.ceil(Math.random() * 150))
-			}
-		})))
+		.pipe(outStream(vehicleName)))
 	// =========================
 	// Question Point 2:
 	// What would happend if it failed to publish to nats or connection to nats is slow?
@@ -86,10 +77,36 @@ const readOutLoud = (vehicleName) => {
 	// =========================
 }
 
+const headersStream = () => { 
+	let s = new Readable();
+	s.push('time,energy,gps,odo,speed,soc\n');
+	s.push(null);
+	return s;
+};
+
+let fsForward = fs.createReadStream("./meta/route-short.csv");
+let fsBackward = fsReverse("./meta/route-short.csv", { matcher: /(\r?\n)/ });
+
+var combinedFw = CombinedStream.create();
+combinedFw.append(headersStream());
+combinedFw.append(fsForward);
+
+var combinedBw = CombinedStream.create();
+combinedBw.append(headersStream());
+combinedBw.append(fsBackward);
+
 // This next few lines simulate Henk's (our favorite driver) shift
 console.log("Henk checks in on test-bus-1 starting his shift...")
-readOutLoud("test-bus-1")
+readOutLoud("test-bus-1", combinedFw)
 	.once("finish", () => {
-		console.log("henk is on the last stop and he is taking a cigarrete while waiting for his next trip")
+		console.log("henk is on the last stop and he is taking a cigarrete while waiting for his next trip");
+
+		readOutLoud("test-bus-1", combinedBw)
+		.once("finish", ()=> {
+			console.log("henk is back at the first stop and he is taking a cigarrete while waiting for his next trip")
+		})
 	})
+	
 // To make your presentation interesting maybe you can make henk drive again in reverse
+
+// done!
